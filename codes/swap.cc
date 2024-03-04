ec4df5b392747797d28816e29991aac2097a1559
Dabhi Urjit <urjitdabhi123@gmail.com>
2024-03-04 22:34:08 +0530

    FIRST EXERCISE COMMIT

---
+++ codes/swap.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std; 
+
+void swap (int *a, int *b ) {
+	int *c = a;
+	*a = *b;
+	*b = c;
+}
+
+int main () {
+   int a  = 10 , b = 20 ;
+   
+
+   cout << a << " " << b << endl;
+   swap(&a,&b);
+   cout << a << " " << b << endl;
+	return -1;
+
+
+}
+
